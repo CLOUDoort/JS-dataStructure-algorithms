e9acf422ca6d039155b8d1dffb140f5343be043c
JunSeok <ppu1234@naver.com>
2023-12-12 13:08:19 +0900

    feat: recursion practice

---
+++ algorithms/Practice/recursion/capitalizeWords.js
@@ -0,0 +1,13 @@
+/**
+ * capitalizeWords라는 재귀 함수를 작성하시오. 단어 배열이 주어지면 각 단어가 대문자로 표시된 새 배열을 반환합니다.
+ */
+
+function capitalizeWords(arr) {
+  if (arr.length === 1) return arr[0].toUpperCase();
+  let newArr = [];
+  newArr.push(arr[0].toUpperCase());
+
+  newArr = newArr.concat(capitalizeWords(arr.slice(1)));
+
+  return newArr;
+}
---
+++ algorithms/Practice/recursion/collectStrings.js
@@ -0,0 +1,32 @@
+/**
+ * 객체를 받아들이고 문자열에 해당하는 모든 값의 배열을 반환하는 collectStrings라는 함수를 작성합니다.
+ */
+
+function collectStrings(obj) {
+  let collect = [];
+  for (let key in obj) {
+    if (typeof obj[key] === 'string') collect.push(obj[key]);
+    else if (typeof obj[key] === 'object') {
+      collect = collect.concat(collectStrings(obj[key]));
+    }
+  }
+  return collect;
+}
+
+function collectStrings(obj) {
+  var stringsArr = [];
+
+  function gatherStrings(o) {
+    for (var key in o) {
+      if (typeof o[key] === 'string') {
+        stringsArr.push(o[key]);
+      } else if (typeof o[key] === 'object') {
+        return gatherStrings(o[key]);
+      }
+    }
+  }
+
+  gatherStrings(obj);
+
+  return stringsArr;
+}
