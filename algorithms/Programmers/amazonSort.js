e36cff851fcd7a6a8f06a336e74b7797d4d112fc
JunSeok <ppu1234@naver.com>
2024-01-02 23:55:52 +0900

    feat: amazon sort

--- algorithms/Programmers/amazonSort.js
+++ algorithms/Programmers/amazonSort.js
@@ -1,16 +1,12 @@
 const makeList = (n) => {
-  return Array.from(Array(n), (v, i) => `a${i + 1}`).concat(Array.from(Array(n), (v, i) => `b${i + 1}`));
-};
-
-const solution = (arr) => {
-  const zip = (a, b) => a.map((v, i) => [v, b[i]]);
-
-  return zip(arr.slice(0, arr.length / 2), arr.slice(arr.length / 2)).flat();
+  return Array.from({ length: n }, (v, i) => `a${i + 1}`).concat(Array.from({ length: n }, (v, i) => `b${i + 1}`));
 };
 
 const amazonSort = (list) => {
   const n = list.length / 2;
+  // 총 n-1번 swap
   for (let i = 1; i < n; i++) {
+    // 1, 3, 5...
     for (let j = i * 2 - 1; j < n + i; j++) {
       [list[j], list[n + i - 1]] = [list[n + i - 1], list[j]];
     }
@@ -19,5 +15,4 @@ const amazonSort = (list) => {
 };
 
 const list = makeList(10);
-console.log(solution(list));
 console.log(amazonSort(list));
