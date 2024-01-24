fcabf8b1bb93dfb50c55ce8750bebbc627347418
JunSeok <ppu1234@naver.com>
2024-01-24 11:37:43 +0900

    refactor: 두 원 사이의 정수 쌍

--- algorithms/Programmers/두 원 사이의 정수 쌍.js
+++ algorithms/Programmers/두 원 사이의 정수 쌍.js
@@ -1,14 +1,10 @@
 function solution(r1, r2) {
   let answer = 0;
-
   for (let x = 1; x <= r2; x++) {
-    const maxY = Math.floor(Math.sqrt(r2 ** 2 - x ** 2));
-    if (r1 > x) {
-      const minY = Math.ceil(Math.sqrt(r1 ** 2 - x ** 2));
-    } else {
-      const minY = 0;
-    }
-    answer += maxY - minY + 1;
+    const y2 = Math.floor(Math.sqrt(r2 ** 2 - x ** 2));
+    const y1 = x >= r1 ? 0 : Math.ceil(Math.sqrt(r1 ** 2 - x ** 2));
+    answer += y2 - y1 + 1;
   }
+
   return answer * 4;
 }
