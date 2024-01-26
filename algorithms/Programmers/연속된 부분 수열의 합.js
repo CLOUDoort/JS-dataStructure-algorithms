5f028fa125f94de5391e81cd2e82b7cdd2a27712
JunSeok <ppu1234@naver.com>
2024-01-26 12:58:41 +0900

    refactor: 연속된 부분 수열의 합(투포인터)

--- algorithms/Programmers/연속된 부분 수열의 합.js
+++ algorithms/Programmers/연속된 부분 수열의 합.js
@@ -1,32 +1,19 @@
 const solution = (sequence, k) => {
-  function solution(sequence, k) {
-    //정답이 될 수 있는 값을 담을 배열
-    const answer = [];
-    let sum = 0;
-    let head = 0;
-    for (let i = 0; i < sequence.length; i++) {
-      sum += sequence[i];
-      if (sum > k) {
-        // 작아질때까지 앞에 값을 빼줌.
-        while (sum > k) {
-          sum -= sequence[head++];
-        }
-      }
-      //같다면 리턴
-      if (sum === k) {
-        answer.push([head, i]);
-      }
+  let st = 0;
+  let en = 0;
+  let sum = sequence[st];
+
+  const result = [];
+  while (en < sequence.length) {
+    if (sum < k) sum += sequence[++en];
+    else if (sum > k) sum -= sequence[st++];
+    else {
+      result.push([st, en]);
+      sum += sequence[++en];
+      sum -= sequence[st++];
     }
-    let min = sequence.length;
-    let result = [];
-    answer.forEach((element) => {
-      if (min > element[1] - element[0]) {
-        min = element[1] - element[0];
-        result = [element[0], element[1]];
-      }
-    });
-    return result;
   }
+  return result.sort((a, b) => a[1] - a[0] - (b[1] - b[0]))[0];
 };
 
-console.log(solution([1, 2, 3, 4, 5]));
+console.log(solution([2, 2, 2, 2, 2], 2));
