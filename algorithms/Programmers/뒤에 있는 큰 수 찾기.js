2003456305fa53238666d4aadf76fd8f5c8c2fab
JunSeok <ppu1234@naver.com>
2024-02-01 14:03:15 +0900

    refactor: 뒤에 있는 큰 수 찾기

--- algorithms/Programmers/뒤에 있는 큰 수 찾기.js
+++ algorithms/Programmers/뒤에 있는 큰 수 찾기.js
@@ -1,29 +1,16 @@
 const solution = (numbers) => {
-  const ans = [];
-  const idx = numbers.findIndex((v, i) => v > numbers[0]);
-  if (idx !== -1) ans.push(numbers[idx]);
-  else ans.push(-1);
+  const answer = Array.from({ length: numbers.length }, () => -1);
+  const stack = [];
 
-  for (let i = 1; i < numbers.length - 1; i++) {
-    const temp = ans.filter((v) => v !== -1).at(-1);
-    console.log(temp);
-    if (temp > numbers[i]) ans.push(temp);
-    else {
-      let flag = false;
-      for (let j = i + 1; j < numbers.length; j++) {
-        if (numbers[j] > numbers[i]) {
-          ans.push(numbers[j]);
-          flag = true;
-          break;
-        }
-      }
-      if (!flag) ans.push(-1);
+  for (let i = 0; i < numbers.length; i++) {
+    while (stack.length && numbers[stack.at(-1)] < numbers[i]) {
+      answer[stack.pop()] = numbers[i];
     }
+    stack.push(i);
   }
-  ans.push(-1);
 
-  return ans;
+  return answer;
 };
 
-console.log(solution([2, 3, 3, 5]));
+// console.log(solution([2, 3, 3, 5]));
 console.log(solution([9, 1, 5, 3, 6, 2]));
