73c8268e5171e697dda4e8a86d23ff9b73f62170
JunSeok <ppu1234@naver.com>
2024-02-02 15:04:47 +0900

    feat: 최댓값과 최솟값

---
+++ algorithms/Programmers/최댓값과 최솟값.js
@@ -0,0 +1,7 @@
+const solution = (s) => {
+  const arr = s.split(' ').map(Number);
+  return Math.min(...arr) + ' ' + Math.max(...arr);
+};
+
+console.log(solution('1 2 3 4'));
+console.log(solution('-1 -2 -3 -4'));
