00e847abdb24cdc389852db95fce9f287f4ec721
JunSeok <ppu1234@naver.com>
2024-02-07 21:14:06 +0900

    카펫

---
+++ algorithms/Programmers/카펫.js
@@ -0,0 +1,10 @@
+const solution = (brown, yellow) => {
+  let x, y;
+  for (y = 3; y <= (brown + yellow) / y; y++) {
+    x = Math.floor((brown + yellow) / y);
+    if ((x - 2) * (y - 2) === yellow) break;
+  }
+  return [x, y];
+};
+
+console.log(solution(24, 24));
