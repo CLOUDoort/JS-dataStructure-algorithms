e4b3664f4056033f030530749b6a540636cc74cf
JunSeok <ppu1234@naver.com>
2024-03-10 22:35:37 +0900

    주사위 고르기

--- algorithms/Programmers/주사위 고르기.js
+++ algorithms/Programmers/주사위 고르기.js
@@ -1,94 +1,92 @@
-const pickDice = (pickSize, length) => {
-  const arr = [];
-  const DFS = (count = [], start = 0) => {
-    if (count.length === pickSize) {
-      arr.push([...count]);
+// 브루트포스
+// 1. 가능한 N/2개의 주사위 조합 생성(주사위 중복 X)
+// 2. 조합별로 주사위를 굴렸을 때 얻을 수 있는 6^(N/2) * 2개의 결과 저장(A, B)
+// 3. A의 모든 n에 대하여 B의 결과에서 n보다 작은 수의 숫자를 모두 더하여 승리 횟수 구하기(이진탐색 활용)
+// 4. 최대 승리 횟수 + 최대로 승리했을 때의 조합 인덱스 저장
+// 5. combination[최대 승리 인덱스]
+
+function solution(dice) {
+  // 1.
+  const N = dice.length;
+  const combination = [];
+  const tmp = [];
+  const dfs = (N, prev = 0) => {
+    if (tmp.length === N / 2) {
+      combination.push([...tmp]);
       return;
     }
-    if (start === length) return;
-    for (let i = start; i < length; i++) {
-      count.push(i);
-      DFS(count, i + 1);
-      count.pop();
+    for (let i = prev + 1; i <= N; i++) {
+      tmp.push(i);
+      dfs(N, i);
+      tmp.pop();
     }
   };
-  DFS();
-  return arr;
-};
+  dfs(N);
 
-const restDice = (arr, length) => {
-  return arr.map((c) => {
-    const b = Array.from({ length }, (_, i) => i);
-    c.forEach((n) => b.splice(b.indexOf(n), 1));
-    return b;
-  });
-};
-
-const simulateCase = (length) => {
-  const caseArr = [];
-  const DFS = (count = []) => {
-    if (count.length === length) {
-      caseArr.push([...count]);
-      return;
-    }
-    for (let i = 0; i < 6; i++) {
-      count.push(i);
-      DFS(count);
-      count.pop();
-    }
+  // 2.
+  const simulateAllDices = (dices) => {
+    const result = [];
+    const roll = (dices, count = 0, sum = 0) => {
+      if (dices.length === count) {
+        result.push(sum);
+        return;
+      }
+      const currentDice = dice[dices[count] - 1];
+      for (let i = 0; i < 6; i++) {
+        roll(dices, count + 1, sum + currentDice[i]);
+      }
+    };
+    roll(dices);
+    return result;
   };
-  DFS();
-  return caseArr;
-};
 
-const solution = (dice) => {
-  const length = dice.length;
-  const pickSize = length / 2;
-  let max = 0;
-  let ans = [];
-
-  const A = pickDice(pickSize, length);
-  const B = restDice(A, length);
-  const caseArr = simulateCase(length);
-
-  const aSum = [];
-  const bSum = [];
-  for (let i = 0; i < A.length; i++) {
-    const temp = [];
-    for (let j = 0; j < caseArr.length; j++) {
-      let sum = 0;
-      A[i].forEach((v, idx) => {
-        sum += dice[v][caseArr[j][idx]];
-      });
-      temp.push(sum);
+  // 3.
+  const binarySearch = (arr, target) => {
+    let left = 0;
+    let right = arr.length;
+    let result = 0;
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      if (arr[mid] < target) {
+        left = mid + 1;
+        result = mid;
+      } else {
+        right = mid - 1;
+      }
     }
-    aSum.push([...temp]);
-  }
-  for (let i = 0; i < B.length; i++) {
-    const temp = [];
-    for (let j = 0; j < caseArr.length; j++) {
-      let sum = 0;
-      B[i].forEach((v, idx) => {
-        sum += dice[v][caseArr[j][idx + pickSize]];
-      });
-      temp.push(sum);
-    }
-    bSum.push([...temp]);
-  }
+    return result;
+  };
 
-  for (let i = 0; i < aSum.length; i++) {
-    let wins = 0;
-    aSum[i].forEach((v, idx) => {
-      if (v > bSum[i][idx]) wins++;
-    });
-    if (wins > max) {
-      max = wins;
-      ans = [...A[i]];
+  // 3.
+  const countWins = (resultA, resultB) => {
+    resultB.sort((a, b) => a - b);
+    let count = 0;
+    for (let num of resultA) {
+      count += binarySearch(resultB, num);
+    }
+    return count;
+  };
+
+  // 4.
+  const allDices = new Array(N).fill().map((_, i) => i + 1);
+  let maxWin = -1;
+  let maxIdx = -1;
+  for (let i = 0; i < combination.length; i++) {
+    const A = combination[i];
+    const aSet = new Set(A);
+    const B = allDices.filter((v) => !aSet.has(v));
+
+    const resultA = simulateAllDices(A);
+    const resultB = simulateAllDices(B);
+
+    const wins = countWins(resultA, resultB);
+    if (wins > maxWin) {
+      maxWin = wins;
+      maxIdx = i;
     }
   }
-
-  return ans.sort((a, b) => a - b).map((i) => i + 1);
-};
+  return combination[maxIdx];
+}
 
 console.log(
   solution([
