f633d844d2fbb3e9af7649f094a9e648a2e6cbd9
JunSeok <ppu1234@naver.com>
2024-03-27 12:03:59 +0900

    두 개 이하로 다른 비트

---
+++ algorithms/Programmers/2개 이하로 다른 비트.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+  const f = (x) => {
+    if (x % 2 === 0) return x + 1;
+    let bit = `0${x.toString(2)}`;
+    let idx = bit.lastIndexOf('0');
+    return parseInt(`${bit.slice(0, idx)}10${bit.slice(idx + 2)}`, 2);
+  };
+  const ans = [];
+  for (let number of numbers) ans.push(f(number));
+  return ans;
+}
